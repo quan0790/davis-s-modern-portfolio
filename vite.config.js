0d384911230197a07db48285b54960fb7882e843
quan0790 <daviesquan8@gmail.com>
2025-12-12 18:19:56 +0300

    modified package json

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 5173,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -25,4 +21,4 @@ export default defineConfig(({ mode }) => ({
     // Increase the chunk size warning limit to 2 MB
     chunkSizeWarningLimit: 10000,
   },
-}));
+});
